ae0b66d95b7c383a0dbbb3d01e9706b78618e341
Phil Plückthun <phil@piemapping.com>
2016-09-22 13:11:29 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vim Adventurous
+
+A dark vim color theme using common colours often seen in the cult series Adventure Time.
+
+![Adventure Time!](http://img1.wikia.nocookie.net/__cb20120811190505/adventuretimewithfinnandjake/images/2/24/Ci_banner_2k12mar20_adventuretime.jpeg)
+
+Based on the [Dracula syntax theme](https://draculatheme.com/).
+
+**Why?** Because Adventure Time's colours are crisp and awesome!
+
+**How?** This is automatically generated! It takes Dracula's colours and matches them with
+a different colour palette.
+
+## Contributing
+
+This is a *work in progress*.
+
+Please report any peculiar color combinations or circumstances under which code is rendered unreadable and ugly.
+Please also try to attach a screenshot, so it is easier to recognise the problem.
---
+++ generate/generate.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const stream = require('stream')
+const Color = require('goethe').default
+const diff = require('color-diff')
+const byline = require('byline')
+const x256 = require('x256')
+
+function colorToDiff(c) {
+  const res = c.toRGB()
+
+  return { R: res.r, G: res.g, B: res.b }
+}
+
+function diffToColor(c) {
+  return Color({ r: c.R, g: c.G, b: c.B })
+}
+
+const palette = [
+  Color('#F8F8F2'), // Normal
+  Color('#191B1F'), // Darkness
+  Color('#404449'), // Ghostly
+  Color('#F5BB12'), // Jake The Dog
+  Color('#FFC620'), // Rawr
+  Color('#D3422E'), // Peppermint Butler
+  Color('#4BAE16'), // Finn's Bag
+  Color('#7fd6fa'), // Ice King
+  Color('#277BD3'), // Finn The Human
+  Color('#f25a55'), // Heartbreaker
+  Color('#de347a'), // Princess Bubblegum
+  Color('#3299CC'), // Adventure Time
+  Color('#8abeb7'), // Cyan
+].map(colorToDiff)
+
+function findSimilarInPalette(c) {
+  const base = colorToDiff(c)
+
+  const aprox = diff.closest(base, palette)
+
+  return diffToColor(aprox)
+}
+
+function colorToTerm(c) {
+  return x256(...c.rgbArray())
+}
+
+function termToColor(code) {
+  if (typeof code !== 'number') {
+    code = parseInt(code)
+  }
+
+  return Color(x256.colors[code])
+}
+
+function transform(str) {
+  const [ key, val ] = str.split('=')
+
+  if (key === 'gui' || key === 'cterm' || val === 'NONE') {
+    return str
+  }
+
+  const isCTerm = key.startsWith('cterm')
+
+  let color
+  if (isCTerm) {
+    color = termToColor(val)
+  } else {
+    color = Color(val)
+  }
+
+  if (!color) {
+    return str
+  }
+
+  const aprox = findSimilarInPalette(color)
+
+  let newVal
+  if (isCTerm) {
+    newVal = colorToTerm(aprox)
+  } else {
+    newVal = aprox.toString('hex')
+  }
+
+  return key + '=' + newVal
+}
+
+class TransformColors extends stream.Transform {
+  _transform(chunk, enc, cb) {
+    const str = chunk.toString()
+
+    if (str.startsWith('hi ')) {
+      const parsed = str.split(' ')
+      const name = parsed[1]
+
+      const rest = parsed
+        .slice(2)
+        .map(x => x.replace(/(\n|\s+)/, ''))
+        .map(transform)
+
+      this.push([
+        'hi',
+        name,
+        ...rest
+      ].join(' ') + '\n')
+    } else {
+      this.push(str + '\n')
+    }
+
+    cb()
+  }
+}
+
+var draculaSyntax = byline.createStream(fs.createReadStream('dracula.vim'))
+var adventurousSyntax = fs.createWriteStream('../colors/adventurous.vim')
+
+draculaSyntax
+  .pipe(new TransformColors())
+  .pipe(adventurousSyntax)
+
